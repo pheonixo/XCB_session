a010080e2053eea9b7ec18b562b105ea499334ef
pheonix <pheonix.sja@att.net>
2025-06-28 07:48:44 -0400

    Default XPD for nfuse to be static, just like nexus default.

--- nfuse.c
+++ nfuse.c
@@ -8,8 +8,10 @@ ui_nfuse_create(PhxInterface *iface, PhxRectangle configure) {
   if (nexus == NULL)  return NULL;
     /* redefine its purpose */
   nexus->type = PHX_NFUSE;
-    /* nfuse is constant width/height. */
-  nexus->state |= HXPD_LFT | VXPD_TOP;
+    /* default: XPD static. Code here only as example.
+      Must always make sure not to affect other bits.
+      ui_nexus_create() has this as default, so unnecessary code. */
+  nexus->state &= ~(HXPD_MSK | VXPD_MSK);
 
   return (PhxNFuse*)nexus;
 }
